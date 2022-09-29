dbd0a667197e02b0992e820a8d021b4d423d534f
CIleProgramlama <104374297+CIleProgramlama@users.noreply.github.com>
2022-09-29 11:48:45 +0300

    Delete Bölüm-8-Fonksiyonlar directory

--- Bölüm-8-Fonksiyonlar/(sayfa_89)hesapla.h
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <math.h>
-
-int UsHesapla(int Xtaban, int Xkuvvet){
-  int Xsonuc = pow(Xtaban, Xkuvvet); //ÜS ALMA İŞLEMİ GERÇEKLEŞTİRİLİYOR
-  printf("%d üzeri %d = %d \n", Xtaban, Xkuvvet, Xsonuc); //SONUÇ EKRANA YAZDIRILIYOR.
-}
