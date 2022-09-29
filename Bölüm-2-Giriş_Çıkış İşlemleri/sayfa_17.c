a516e7f4013ba12da3e98fed4e8fe9ecd3b6811d
CIleProgramlama <104374297+CIleProgramlama@users.noreply.github.com>
2022-09-29 11:47:44 +0300

    Delete Bölüm-2-Giriş_Çıkış İşlemleri directory

--- Bölüm-2-Giriş_Çıkış İşlemleri/sayfa_17.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>    
-int main()
-{
-    //İçerisinde yazan metin ifadeyi gösterir
-    printf("C programlama dili");
-    return 0;
-}
--- Bölüm-2-Giriş_Çıkış İşlemleri/sayfa_18.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-    int rakam = 5;
-    printf("rakam = %d", rakam);
-    return 0;
-}
