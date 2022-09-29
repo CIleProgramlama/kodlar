b9bc4512df362f020e57cebea363d79e55b0ec5b
CIleProgramlama <104374297+CIleProgramlama@users.noreply.github.com>
2022-09-29 11:19:55 +0300

    Add files via upload

---
+++ Bölüm-2-Giriş_Çıkış İşlemleri/sayfa_18.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+    int rakam = 5;
+    printf("rakam = %d", rakam);
+    return 0;
+}
---
+++ Bölüm-2-Giriş_Çıkış İşlemleri/sayfa_21.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    char ogrenciAdi[] = "Ahmet";
+    long int numarasi = 123456789;
+    int dogumGunu = 23;
+
+    printf("Isim=%s \nOgrenci numarasi: %ld \t Dogum gunu:%d",ogrenciAdi,numarasi,dogumGunu);
+    return 0;
+}
