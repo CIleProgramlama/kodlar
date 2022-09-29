0766aef4d69b3403efd36fbee76e69a00093a4be
CIleProgramlama <104374297+CIleProgramlama@users.noreply.github.com>
2022-09-29 11:48:36 +0300

    Delete Bölüm-7-Diziler directory

--- Bölüm-7-Diziler/sayfa_77.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-int main() {
-  char *haftanin_gunleri[7] = {"Pazartesi","Salı","Çarsamba","Perşembe","Cuma","Cumartesi","Pazar"};
-  for(int i=0; i<7; i++)
-  printf("%d. gün %s \n", i+1, haftanin_gunleri[i]);
-  return 0;
-}
